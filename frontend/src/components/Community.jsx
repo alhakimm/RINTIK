f1bb1e282f4d21876991a7e5a9d76dafcedc12e2
alhakimm <alhakim1519@gmail.com>
2023-12-25 23:18:17 +0800

    update community

--- frontend/src/components/Community.jsx
+++ frontend/src/components/Community.jsx
@@ -1,86 +1,51 @@
 import React from 'react'
 import { useState } from 'react'
 import { IoPersonCircleSharp } from "react-icons/io5"
+import { FaTelegramPlane } from "react-icons/fa";
 import logo from '../assets/logo.png';
 import PostCommunity from './PostCommunity';
 
 const Community = () => {
 
-    const [comments, setComments] = useState({});
+    // const [comments, setComments] = useState({});
 
-  const upvote = (postId) => {
-    alert(`Upvoted post ${postId}`);
-    // You can add logic to update the upvote count or perform other actions.
-  };
+    // const upvote = (postId) => {
+    //   alert(`Upvoted post ${postId}`);
+    //   // You can add logic to update the upvote count or perform other actions.
+    // };
 
-  const handleComment = (postId) => {
-    const commentText = prompt('Enter your comment:');
-    if (commentText) {
-      setComments((prevComments) => ({
-        ...prevComments,
-        [postId]: [...(prevComments[postId] || []), commentText],
-      }));
-    }
-  };
+    // const handleComment = (postId) => {
+    //   const commentText = prompt('Enter your comment:');
+    //   if (commentText) {
+    //     setComments((prevComments) => ({
+    //       ...prevComments,
+    //       [postId]: [...(prevComments[postId] || []), commentText],
+    //     }));
+    //   }
+    // };
 
   return (
     <div>
-      <div className='bg-gradient-to-r from-[#0A2236] to-[#15436E] w-full flex px-20 justify-between h-screen gap-2'>
+      <div className='bg-gradient-to-r from-[#0A2236] to-[#15436E] w-full flex px-20 justify-between h-full gap-2'>
         <div className='grid grid-cols-5 flex-1 mt-6'>
           <div className='w-full col-span-1 pr-6'>
             <div className='h-1/4 bg-red-200'>weather</div>
             <div className='h-2/4 bg-green-200 my-2'>joined group</div>
           </div>
+        {/* start of community post */}
           <div className='w-full col-span-3'>
-            <div className="bg-white rounded-lg shadow-md w-full p-4">
-              <div className="flex space-x-4">
-              <IoPersonCircleSharp size={45} />
-                <div className="flex-1">
-                  <textarea
-                    placeholder="Start a post..."
-                    className="w-full h-20 resize-none border rounded-md p-2 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                  ></textarea>
-                </div>
-              </div>
-              <div className="flex justify-end mt-4">
-                <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md focus:outline-none">
-                  Post
-                </button>
+
+            <div className="bg-white rounded-3xl shadow-md w-full p-4">
+              <div className='flex items-center'>
+                <IoPersonCircleSharp size={40} className='mr-2' />
+                <textarea name="" id="" placeholder='Post your twit here' className='w-full resize-none border-2 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500'></textarea>
+                <button className='text-[#15436E] p-2'><FaTelegramPlane size={20}/></button>
               </div>
             </div>
-
-            <div className=''>
-              <section id="forum">
-                <div className="post mt-7 rounded-lg bg-white p-5">
-                  <p className='text-xl font-bold'>Post Title 1</p>
-                  <div className="post-body">Post 1. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</div>
-                  <button onClick={() => upvote(1)}>Upvote</button>
-                  <button onClick={() => handleComment(1)}>Comment</button>
-                  <div id="comments-1" className="comments">
-                    {comments[1] && comments[1].map((comment, index) => (
-                      <div key={index} className="comment">{comment}</div>
-                    ))}
-                  </div>
-                </div>
-
-                <div className="post">
-                  <div className="post-title">Post Title 2</div>
-                  <div className="post-body">Post 2. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</div>
-                  <button onClick={() => upvote(2)}>Upvote</button>
-                  <button onClick={() => handleComment(2)}>Comment</button>
-                  <div id="comments-2" className="comments">
-                    {comments[2] && comments[2].map((comment, index) => (
-                      <div key={index} className="comment">{comment}</div>
-                    ))}
-                  </div>
-                </div>
-              </section>
-        </div>
-            
             
             <div><PostCommunity/></div>
-            <div>comunity</div>
           </div>
+          {/* end of community post */}
           <div className='w-full col-span-1 pl-6'>
             <div className='h-1/4 bg-red-200'>alert</div>
             <div className='h-2/4 bg-green-200 my-2'>autoscroll news</div>
--- frontend/src/components/PostCommunity.jsx
+++ frontend/src/components/PostCommunity.jsx
@@ -1,15 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios'
+import { IoPersonCircleSharp } from "react-icons/io5";
+import { FaRegComments } from "react-icons/fa";
+import { BiUpvote } from "react-icons/bi";
 
 export class PostCommunity extends Component {
     state = {
         posts: null
     }
     componentDidMount(){
-        console.log('sini sampai')
         axios.get('http://localhost:5000/testingfirebase-3e0f7/us-central1/api/posts')
             .then(res => {
-                
                 console.log(res.data)
                 this.setState({
                     posts: res.data
@@ -19,19 +20,23 @@ export class PostCommunity extends Component {
     }
   render() {
     let postFinder = this.state.posts ? (
-        this.state.posts.map(posts => <div key={posts.id} className="posts-box">
-          <p> {posts.body}</p>
-          <p> {posts.username}</p>
-          <p> {posts.createdAt}</p>
-          <p> {posts.comments}</p>
-          <p> {posts.upvote}</p>
+        this.state.posts.map(posts => <div key={posts.id} className="posts-box bg-white mb-4 p-4 rounded-3xl">
+          <div className='flex items-center'>
+            <IoPersonCircleSharp size={30} className='mr-4'/>
+            <p className='mr-4 font-semibold'> {posts.username}</p>
+            <p className='text-gray-400'> {posts.createdAt}</p>
+          </div>
+          <p className='mb-2'> {posts.body}</p>
+          <div className='flex gap-2'>
+            <button className='border rounded-full flex items-center px-2 bg-[#15436E] text-white'><FaRegComments className='mr-2'/> {posts.comments}</button>
+            <button className='border rounded-full flex items-center px-2 bg-[#15436E] text-white'><BiUpvote className='mr-2'/> {posts.upvote}</button>
+          </div>
       </div>
     )) : <p>loading...</p>
 
     return (
       <div>
         <h1>hello this is postcommunity</h1>
-        
         {postFinder}
       </div>
     )
