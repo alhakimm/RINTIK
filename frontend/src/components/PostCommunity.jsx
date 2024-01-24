abf15185fd091aaf7ac7fe8b2eefe82f6b12f0ba
alhakimm <alhakim1519@gmail.com>
2024-01-25 04:40:32 +0800

    joined group ui

--- frontend/src/components/PostCommunity.jsx
+++ frontend/src/components/PostCommunity.jsx
@@ -45,22 +45,21 @@ export class PostCommunity extends Component {
     let postFinder = this.state.posts ? (
         this.state.posts.map(posts => <div key={posts.id} className="posts-box bg-white mb-4 p-4 rounded-3xl">
           <div className='flex items-center'>
-            <IoPersonCircleSharp size={30} className='mr-4'/>
-            <p className='mr-4 font-semibold'> {posts.username}</p>
+            <IoPersonCircleSharp size={40} className='mr-4 text-blue-500'/>
+            <p className='mr-4 font-semibold text-lg'> {posts.username}</p>
             <p className='text-gray-400'> {posts.createdAt}</p>
           </div>
           <p className='mb-2'> {posts.body}</p>
           <div className='flex gap-2'>
-            <button className='border rounded-full flex items-center px-2 bg-[#15436E] text-white'><FaRegComments className='mr-2'/> {posts.comments}</button>
-            <button className='border rounded-full flex items-center px-2 bg-[#15436E] text-white' onClick={() => this.handleUpvote(posts.id)}><BiUpvote className='mr-2'/> {posts.upvote}</button>
+            <button className='border rounded-full flex items-center px-2 bg-blue-500 text-white'><FaRegComments className='mr-2'/> {posts.comments}</button>
+            <button className='border rounded-full flex items-center px-2 bg-blue-500 text-white' onClick={() => this.handleUpvote(posts.id)}><BiUpvote className='mr-2'/> {posts.upvote}</button>
           </div>
       </div>
     )) : <p>loading...</p>
 
     return (
-      <div>
-        <h1>hello this is postcommunity</h1>
-        {postFinder}
+      <div className='flex flex-col'>
+         {postFinder}
       </div>
     )
   }
